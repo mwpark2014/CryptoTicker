09796590aa45eb9215c798a40341f095dea690ef
Mason <park.mason.w@gmail.com>
2018-01-09 21:52:56 -0500

    Got rid of some default code

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ class App extends Component {
         <div className="App-header">
           <h2>Cryptocurrency Ticker</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
